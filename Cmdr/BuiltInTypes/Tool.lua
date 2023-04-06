5d62f45237bd1fa67627c9b398389f67b39ac768
StrongSpider <spiider.spiderco@gmail.com>
2023-04-05 21:55:55 -0400

    Create Tool.lua

---
+++ Cmdr/BuiltInTypes/Tool.lua
@@ -0,0 +1,28 @@
+local Util = require(script.Parent.Parent.Shared.Util)
+
+local toolType = {
+	Transform = function (text)
+		local findTool = Util.MakeFuzzyFinder(game.ReplicatedStorage.ToolClone:GetChildren())
+		return findTool(text)
+	end;
+
+	Validate = function (tools)
+		return #tools > 0, "No tool with that name could be found."
+	end;
+
+	Autocomplete = function (tools)
+		return Util.GetNames(tools)
+	end;
+
+	Parse = function (tools)
+		return tools[1]
+	end;
+
+	Default = function(tool)
+		return tool.Name
+	end;
+}
+
+return function (cmdr)
+	cmdr:RegisterType("tool", toolType)
+end
